47d2e767306538e645dd994d796cb22e9c8cf79c
GoodOBag <goodobag@gmail.com>
2016-03-08 21:12:12 -0800

    [Develop] Modify purchase table and add status table

--- main.go
+++ main.go
@@ -28,10 +28,25 @@ func main() {
 	checkErr(err) //20160215, Harrison St. Senior House, 1000 Harrison St., 94555, good place
 	_, err = db.Exec("create table customertable (id integer not null primary key autoincrement, regdate integer not null, nickname text not null, phone integer not null, bldg text not null, room text not null, notes text);")
 	checkErr(err) //1, 20160215, John-HS-301, 5105698756, Harrison St. Senior House, 301A, good guy
-	_, err = db.Exec("create table ordertable (id integer not null primary key autoincrement, nickname text not null, orderdate integer not null, orderlist text not null);")
-	checkErr(err) //1, John-HS-301, 20160216, apple/ea,5,;banana/lb,1.5,green
-	_, err = db.Exec("create table purchasetable (id integer not null primary key autoincrement, purchasedate integer not null, purchaselist text not null);")
-	checkErr(err) //1, 20160216, apple/ea,12;banana/lb,2.9
-	/*_, err = db.Exec("create table  (id integer not null primary key, );")
-	  checkErr(err)*/
+	_, err = db.Exec("create table ordertable (id integer not null primary key autoincrement, nickname text not null, orderdate integer not null, orderlist text not null, status integer not null);")
+	checkErr(err) //1, John-HS-301, 20160216, apple/ea,5,,0;banana/lb,1.5,green,1
+	_, err = db.Exec("create table purchasetable (id integer not null primary key autoincrement, purchasedate integer not null, item text not null, unit text not null, amount real not null, status integer not null);")
+	checkErr(err) //1, 20160216, apple, ea, 12, 0
+	//status = 0 -> not submitted; status > 0 -> submitted
+	_, err = db.Exec("create table status (indx integer not null, purchaseLastId integer not null, orderLastId integer not null);")
+	checkErr(err) //23, 79, 490
+	//last report was 23rd report; last id in purchase table was 79; last id in order table was 490
+
+	ini()
+}
+
+func ini() {
+	db, err := sql.Open("sqlite3", "./record.db")
+	checkErr(err)
+	defer db.Close()
+
+	stmt, err := db.Prepare("insert into status(indx, purchaseLastId, orderLastId) values(?,?,?)")
+	checkErr(err)
+	_, err = stmt.Exec(0, 0, 0)
+	checkErr(err)
 }
