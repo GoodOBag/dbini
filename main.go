14dc1271e3ce2f8eca14f3515122bf92adb623ac
GoodOBag <goodobag@gmail.com>
2016-04-02 15:19:25 -0700

    [Develop] Finalize db structure

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ func checkErr(err error) {
 }
 
 func main() {
+	recordIni()
+	reportIni()
+}
+
+func recordIni() {
 	os.Remove("./record.db")
 	db, err := sql.Open("sqlite3", "./record.db")
 	checkErr(err)
@@ -29,24 +34,20 @@ func main() {
 	_, err = db.Exec("create table customertable (id integer not null primary key autoincrement, regdate integer not null, nickname text not null, phone integer not null, bldg text not null, room text not null, notes text);")
 	checkErr(err) //1, 20160215, John-HS-301, 5105698756, Harrison St. Senior House, 301A, good guy
 	_, err = db.Exec("create table ordertable (id integer not null primary key autoincrement, nickname text not null, orderdate integer not null, orderlist text not null, status integer not null);")
-	checkErr(err) //1, John-HS-301, 20160216, apple/ea,5,,0;banana/lb,1.5,green,1
-	_, err = db.Exec("create table purchasetable (id integer not null primary key autoincrement, purchasedate integer not null, item text not null, unit text not null, amount real not null, status integer not null);")
-	checkErr(err) //1, 20160216, apple, ea, 12, 0
+	checkErr(err) //1, John-HS-301, 20160216, item ^ unit ^ amount ^ notes ^ unit2 ^ amount2 ^ price? ... , 0
+	_, err = db.Exec("create table purchasetable (id integer not null primary key autoincrement, purchasedate integer not null, item text not null, unit text not null, amount real not null, price real not null, status integer not null);")
+	checkErr(err) //1, 20160216, apple, ea, 12, 50, 0
 	//status = 0 -> not submitted; status > 0 -> submitted
-	_, err = db.Exec("create table status (indx integer not null);")
-	checkErr(err) //23
-
-	ini()
+	_, err = db.Exec("create table tempReport (date integer not null, purchaseList text not null, orderList text not null)")
+	checkErr(err)
 }
 
-func ini() {
-	db, err := sql.Open("sqlite3", "./record.db")
+func reportIni() {
+	os.Remove("./report.db")
+	db, err := sql.Open("sqlite3", "./report.db")
 	checkErr(err)
 	defer db.Close()
 
-	stmt, err := db.Prepare("insert into status(indx) values(?)")
+	_, err = db.Exec("create table reports (id integer not null primary key autoincrement, date integer not null, purchaseList text not null, orderList text not null)")
 	checkErr(err)
-	_, err = stmt.Exec(1) //change 1 to other number if want to start with a different number. Other tables are empty
-	checkErr(err)
-
 }
